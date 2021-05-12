a9610404526a9df5aa19d8eb00f9eb9b6b7bd79d
ezeilo-su <ezeilo.su@gmail.com>
2021-05-12 22:29:11 +0100

    Use references to relate documents

--- vidly/population.js
+++ vidly/population.js
@@ -1,24 +1,29 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/playground')
+mongoose.connect('mongodb://localhost/playground',
+  { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB...'))
-  .catch(err => console.error('Could not connect to MongoDB...', err));
+  .catch((err) => console.error('Could not connect to MongoDB...', err));
 
 const Author = mongoose.model('Author', new mongoose.Schema({
   name: String,
   bio: String,
-  website: String
+  website: String,
 }));
 
 const Course = mongoose.model('Course', new mongoose.Schema({
   name: String,
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Author',
+  },
 }));
 
-async function createAuthor(name, bio, website) { 
+async function createAuthor(name, bio, website) {
   const author = new Author({
-    name, 
-    bio, 
-    website 
+    name,
+    bio,
+    website,
   });
 
   const result = await author.save();
@@ -27,23 +32,24 @@ async function createAuthor(name, bio, website) {
 
 async function createCourse(name, author) {
   const course = new Course({
-    name, 
-    author
-  }); 
-  
+    name,
+    author,
+  });
+
   const result = await course.save();
   console.log(result);
 }
 
-async function listCourses() { 
+async function listCourses() {
   const courses = await Course
     .find()
-    .select('name');
+    .populate('author', 'name bio -_id')
+    .select('name author');
   console.log(courses);
 }
 
-createAuthor('Mosh', 'My bio', 'My Website');
+// createAuthor('Mosh', 'My bio', 'My Website');
 
-// createCourse('Node Course', 'authorId')
+// createCourse('Node Course', '609c401a0030a3884f6f6227');
 
-// listCourses();
+listCourses();
