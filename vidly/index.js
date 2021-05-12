9e1ea8e4fa7bdc0919c1ca7dd0478dd8cecfe5a1
ezeilo-su <ezeilo.su@gmail.com>
2021-05-12 15:33:56 +0100

    Finish Refactor genres to use MongoDB

--- vidly/index.js
+++ vidly/index.js
@@ -1,10 +1,25 @@
-const Joi = require('joi');
+const debug = require('debug')('app:startup');
+const logger = require('./middleware/logger');
+const mongoose = require('mongoose');
 const genres = require('./routes/genres');
+const morgan  = require('morgan');
 const express = require('express');
 const app = express();
 
+mongoose.connect('mongodb://localhost/vidly', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(() => console.error('Could not connect to MongoDB'));
+
 app.use(express.json());
 app.use('/api/genres', genres);
+app.use(logger);
+
+
+if(app.get('env') === "development") {
+  app.use(morgan('tiny'));
+  debug('Morgan enabled...');
+}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
+
