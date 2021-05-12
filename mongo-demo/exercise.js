7abc4117c0f0bbad0f926b216519da8e1e914941
ezeilo-su <ezeilo.su@gmail.com>
2021-05-12 12:43:00 +0100

    Finish validation lesson

--- mongo-demo/exercise.js
+++ mongo-demo/exercise.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/mongo-exercises')
+mongoose.connect('mongodb://localhost/mongo-exercises',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }
+  )
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Error...', err));
 
@@ -16,10 +21,69 @@ const courseSchema = new mongoose.Schema({
 const Course = mongoose.model('Course', courseSchema);
 
 async function getCourses() {
-  return await Course.find({ tags: 'backend', isPublished: true })
-    .sort({ name: 1 })
-    .select({ name: 1, author: 1 });
+  // return await Course.find(
+  //     { tags: 'backend', isPublished: true },
+  //     { name: 1, author: 1, _id: 0 }
+  //   )
+  //   .sort({ name: 1 })
+
+  // return await Course.find(
+  //     {
+  //       tags: { $in: ['frontend', 'backend'] },
+  //       isPublished: true 
+  //     },
+  //     'name author price -_id'
+  //   )
+  //   .sort('-price')
+
+  return await Course.find(
+      { isPublished: true },
+      'name author price -_id'
+    )
+    .or([
+      { tags: 'frontend' },
+      { tags: 'backend' }
+    ])
+    .sort('-price');
 }
 
-getCourses()
-  .then(res => console.log(res));
+
+
+// getCourses()
+//   .then(res => console.log(res.map(e => e._doc)));
+
+// async function updateCourse(id) {
+//   const course = await Course.findById(id);
+//   if(!course) {
+//     console.log('course not found');
+//     return;
+//   }
+
+//   // course.isPublished = true;
+//   // course.author = 'Another Author';
+//   // OR
+//   course.set({
+//     isPublished: true,
+//     author: 'A Different Author'
+//   });
+
+//   const result = await course.save();
+//   console.log(result);
+// }
+
+async function updateCourse(id) {
+  const result = await Course.updateOne(
+    { _id: id },
+    { isPublished: true, author: 'Different Author' }
+  )
+
+  console.log(result);
+}
+
+async function removeCourse(id) {
+  const result = await Course.deleteOne({ _id: id })
+
+  console.log(result);
+}
+
+removeCourse('609b83875ed801861846b7d8');
