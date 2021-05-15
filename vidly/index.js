56e3749c5f2242a917a1a1af93bfcce8ad02e9fb
ezeilo-su <ezeilo.su@gmail.com>
2021-05-15 08:40:59 +0100

    Finish refactor code

--- vidly/index.js
+++ vidly/index.js
@@ -4,17 +4,18 @@ const express = require('express');
 const logger = require('./startup/logging');
 const app = express();
 
-require('./startup/config')();
-require('./startup/routes')(app);
-require('./startup/db')();
-require('./startup/logging')();
-require('./startup/validation')();
-
 
 if (app.get('env') === 'development') {
   app.use(morgan('tiny'));
-  debug('Morgan enabled...');
+  logger.info('Morgan enabled...');
 }
+
+require('./startup/routes')(app);
+require('./startup/config')();
+require('./startup/db')();
+require('./startup/logging');
+require('./startup/validation')();
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => logger.info(`Listening on port ${port}...`));
--- vidly/startup/db.js
+++ vidly/startup/db.js
@@ -3,6 +3,9 @@ const logger = require('./logging');
 
 module.exports = function() {  
   mongoose.connect('mongodb://localhost/vidly',
-  { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+    { useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    })
     .then(() => logger.info('Connected to MongoDB...'))
 }
