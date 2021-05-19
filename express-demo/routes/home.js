6be1789ef5c0da97342aa1cac0f70e888b593faa
ezeilo-su <ezeilo.su@gmail.com>
2021-05-19 09:05:35 +0100

    Move vidly to a unique repo

--- express-demo/routes/home.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  // res.send('Hello world');
-  res.render('index', { title: 'My Express App', message: 'Hello, world!' });
-})
-
-module.exports = router;
