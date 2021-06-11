5ce7d0795ea3df6e5380a8b62da0d93790700ddc
ezeilo-su <ezeilo.su@gmail.com>
2021-06-11 16:45:42 +0100

    Refactor auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,11 +3,10 @@ const jwt = require('jsonwebtoken');
 const { User } = require('../models/user');
 
 module.exports = async (req, res, next) => {
-  console.log(req.headers);
-  const token = req.headers['authorization'].split(' ')[1];
-  if (!token) return res.status(401).send('Access denied. No token provided');
+  const bearerToken = req.header('authorization');
+  if (!bearerToken) return res.status(401).send('Access denied. No token provided');
   try {
-    const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
+    const decoded = jwt.verify(bearerToken.split(' ')[1], config.get('jwtPrivateKey'));
     const user = await User.findById(decoded._id);
     if(!user) return res.status(401).send('Access denied.');
     req.user = decoded;
