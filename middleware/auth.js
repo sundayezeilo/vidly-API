329d93ff096f0edf77f54a7af4ed0ffe727e785e
ezeilo-su <ezeilo.su@gmail.com>
2021-06-11 16:39:41 +0100

    Rename x-auth-token key in the auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,11 +1,15 @@
 const config = require('config');
 const jwt = require('jsonwebtoken');
+const { User } = require('../models/user');
 
-module.exports = (req, res, next) => {
-  const token = req.header('x-auth-token');
+module.exports = async (req, res, next) => {
+  console.log(req.headers);
+  const token = req.headers['authorization'].split(' ')[1];
   if (!token) return res.status(401).send('Access denied. No token provided');
   try {
     const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
+    const user = await User.findById(decoded._id);
+    if(!user) return res.status(401).send('Access denied.');
     req.user = decoded;
   } catch (ex) {
     return res.status(400).send('Invalid token.');
