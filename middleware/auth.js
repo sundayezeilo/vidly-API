74e86de51eb67a3599e1886002845c368b6d8022
ezeilo-su <ezeilo.su@gmail.com>
2021-06-30 20:11:15 +0100

    Restore x-auth-token header name

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const { User } = require('../models/user');
 
 module.exports = async (req, res, next) => {
-  const bearerToken = req.header('authorization');
+  const bearerToken = req.header('x-auth-token');
   if (!bearerToken) return res.status(401).send('Access denied. No token provided');
   try {
     const decoded = jwt.verify(bearerToken.split(' ')[1], config.get('jwtPrivateKey'));
